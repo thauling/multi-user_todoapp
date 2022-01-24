e3b66c8a38a61b6aca5d790626ee5a9a802973a9
thauling <hauling.thomas@gmail.com>
2022-01-24 20:25:28 +0000

    test tailwind, trial&error with tailwind classes

--- src/header.php
+++ src/header.php
@@ -4,15 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+   <!-- <link rel="stylesheet" href="style.css">
+    <link href="/dist/output.css" rel="stylesheet"> -->
+    <link href="output.css" rel="stylesheet"> 
     <title>Lots of stuff to do!</title>
 </head>
-<body>
+<body class="text-gray-600">
 <header>
-    <h1>Lots of stuff to do!</h1>
-    <nav class="navbar-main">
+    <h1 class="text-sky-500 dark:text-sky-400 text-3xl font-bold uppercase">Lots of stuff to do!</h1>
+    <nav class="text-gray-600">
         <ul>
-            <li><a href="index.php">Home</a></li>
+            <li class="font-bold"><a href="index.php">Home</a></li>
             <li><a href="tasks.php">Tasks</a></li>
         </ul>
     </nav>
--- src/index.php
+++ src/index.php
@@ -10,21 +10,22 @@ $pswrepeat = '';
 ?>
 
 <body>
-    </table>
+    <div class="inline-block p-6 bg-gradient-to-b from-green-400 to-blue-400">
+    <div class="create bg-gray-100 h-5">
+        <table class="table">
+            <form method="post" action="index.php">
+                <input type="hidden" name="user_id" value="<?php echo $user_id; ?>" />
+                <label for="username">Come up with a username (8 characters minimum):</label>
+                <input type="text" name="username" value="<?php echo $username ?>" placeholder="Enter a username" minlength="8" required />
+                <label for="password">Enter your email:</label>
+                <input type="email" name="email" value="<?php echo $email ?>" placeholder="Enter your email" required />
+                <label for="password">Password (8 characters minimum):</label>
+                <input type="password" name="password" value="<?php echo $password ?>" placeholder="Enter a password" minlength="8" required />
+                <label for="pswrepeat">Repeat password:</label>
+                <input type="password" name="pswrepeat" value="<?php echo $pswrepeat ?>" placeholder="Repeat your password" minlength="8" required />
+                <button type="submit" name="submit">Submit</button>
+            </form>
     </div>
-    <div class="create">
-        <form method="post" action="index.php">
-            <input type="hidden" name="user_id" value="<?php echo $user_id; ?>" />
-            <label for="username">Come up with a username (8 characters minimum):</label>
-            <input type="text" name="username" value="<?php echo $username ?>" placeholder="Enter a username" minlength="8" required />
-            <label for="password">Enter your email:</label>
-            <input type="email" name="email" value="<?php echo $email ?>" placeholder="Enter your email" required />
-            <label for="password">Password (8 characters minimum):</label>
-            <input type="password" name="password" value="<?php echo $password ?>" placeholder="Enter a password" minlength="8" required />
-            <label for="pswrepeat">Repeat password:</label>
-            <input type="password" name="pswrepeat" value="<?php echo $pswrepeat ?>" placeholder="Repeat your password" minlength="8" required />
-            <button type="submit" name="submit">Submit</button>
-        </form>
     </div>
 </body>
 <?php
