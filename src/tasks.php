77602ddd71e70f7bf12b43a76f07091fb8190c48
thauling <hauling.thomas@gmail.com>
2022-01-25 19:24:10 +0000

    tweak styles in tasks.php

--- src/tasks.php
+++ src/tasks.php
@@ -76,56 +76,64 @@ $rows = getAllTasks($conn);
 </head>
 
 <body>-->
-    <div class="h-full w-full p-4 border-4 border-teal-400 bg-gray-100">
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Created</th>
-                    <th>Done?</th>
-                </tr>
-            </thead>
-            <?php
-            foreach ($rows as $row) : ?>
-                <tr>
-                    <td><?= $row['task_id']; ?></td>
-                    <td><?= $row['title']; ?></td>
-                    <td><?= $row['description']; ?></td>
-                    <!-- <td><?= $row['completed']; ?></td> -->
-                    <td><?= $row['created']; ?></td>
-                    <td><?php if ($row['completed'] == -1) {echo 'nope';} else {echo 'bingo!';} ?></td>
-                    <td>
-                        <a href="tasks.php?edit=<?php echo $row['task_id']; ?>">Edit</a> <!-- needs to be GET method-->
-                        <a href="tasks.php?delete=<?php echo $row['task_id']; ?>">Delete</a> <!-- needs to be GET method-->
-                        <a href="tasks.php?completed=<?php echo $row['task_id']; ?>">Completed!</a> <!-- needs to be GET method-->
+<div class="h-full w-full m-0 p-0 border-2 border-teal-400 bg-gray-100">
+    <table class="bg-gray-300 m-0 p-0 table w-full">
+        <thead class="table-header-group">
+            <tr class="table-row">
+                <th class="bg-teal-400 border text-left px-1 py-1">ID</th>
+                <th class="bg-teal-400 border text-left px-1 py-1">Title</th>
+                <th class="bg-teal-400 border text-left px-1 py-1">Description</th>
+                <th class="bg-teal-400 border text-left px-1 py-1">Created</th>
+                <th class="bg-teal-400 border text-left px-1 py-1">Done?</th>
+            </tr>
+        </thead>
+        <?php
+        foreach ($rows as $row) : ?>
+            <tr class="table-row">
+                <td class="border border-gray-400 text-teal-900 table-cell bg-gray-200"><?= $row['task_id']; ?></td>
+                <td class="border border-gray-400 text-teal-900 table-cell bg-gray-200"><?= $row['title']; ?></td>
+                <td class="border border-gray-400 text-teal-900 table-cell bg-gray-200"><?= $row['description']; ?></td>
+                <!-- <td><?= $row['completed']; ?></td> -->
+                <td class="border border-gray-400 text-teal-900 table-cell bg-gray-200"><?= $row['created']; ?></td>
+                <td class="border border-gray-400 text-teal-900 table-cell bg-gray-200"><?php if ($row['completed'] == -1) {
+                                                                        echo 'nope';
+                                                                    } else {
+                                                                        echo 'bingo!';
+                                                                    } ?></td>
+                <td>
+                    <a class="bg-gray-700 text-gray-200 mt-2 w-56 text-center" href="tasks.php?edit=<?php echo $row['task_id'];
+                                                                                                    ?>">Edit</a> <!-- needs to be GET method-->
+                    <a class="bg-red-700 text-gray-200 mt-2 w-56 text-center" href="tasks.php?delete=<?php echo $row['task_id'];
+                                                                                                        ?>">Delete</a> <!-- needs to be GET method-->
+                    <a class="bg-green-700 text-gray-200 mt-2 w-56 text-center" href="tasks.php?completed=<?php echo $row['task_id'];
+                                                                                                            ?>">Complete</a> <!-- needs to be GET method-->
 
-                        <!-- <input type="checkbox" name="completed" id="completed"> 
+                    <!-- <input type="checkbox" name="completed" id="completed"> 
         <label for="completed">Completed:</label> -->
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-    </div>
-    <div class="create">
-        <form method="post" action="tasks.php">
-            <input type="hidden" name="task_id" value="<?php echo $task_id; ?>" />
-            <label for="title">Write a title for your task:</label>
-            <input type="text" name="title" value="<?php echo $title ?>" placeholder="Title for task" required />
-            <label for="description">Write a description for your task:</label>
-            <input type="text" name="description" value="<?php echo $description ?>" placeholder="Description for task" required />
-            <?php
-            if ($edit) :
-                $edit = false;
-                //echo $title; ?>
-                <button type="submit" name="update">Update</button>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</div>
+<div class="h-full w-full m-1 p-1 border-1 border-gray-400 bg-gray-100">
+    <form class="flex flex-col content-center justify-items-center" method="post" action="tasks.php">
+        <input type="hidden" name="task_id" value="<?php echo $task_id; ?>" />
+        <label for="title">Write a title for your task:</label>
+        <input type="text" name="title" value="<?php echo $title ?>" placeholder="Title for task" required />
+        <label for="description">Write a description for your task:</label>
+        <input type="text" name="description" value="<?php echo $description ?>" placeholder="Description for task" required />
+        <?php
+        if ($edit) :
+            $edit = false;
+            //echo $title; 
+        ?>
+            <button class="bg-teal-700 text-gray-200 mt-2 w-56 text-center" type="submit" name="update">Update</button>
 
-            <?php else : ?>
-                <button type="submit" name="save">Save</button>
-            <?php endif; ?>
-        </form>
-    </div>
+        <?php else : ?>
+            <button class="bg-teal-700 text-gray-200 mt-2 w-56 text-center" type="submit" name="save">Save</button>
+        <?php endif; ?>
+    </form>
+</div>
 </body>
 <?php
 require_once 'footer.php';
